770c14b9845ce92dc30a4dc37f0b99b453db9cb3
Johannes Schlatow <schlatow@ida.ing.tu-bs.de>
2016-07-05 11:51:45 +0200

    add remote_rom proxy component
    relates to #20

---
+++ include/remote_rom/backend_base.h
@@ -0,0 +1,33 @@
+/*
+ * \brief  Backend interface used by the Remote_rom client and server
+ * \author Johannes Schlatow
+ * \date   2016-02-17
+ */
+
+#ifndef __INCLUDE__REMOTE_ROM__BACKEND_BASE_H_
+#define __INCLUDE__REMOTE_ROM__BACKEND_BASE_H_
+
+#include <rom_forwarder.h>
+#include <rom_receiver.h>
+
+namespace Remote_rom {
+	struct Backend_server_base;
+	struct Backend_client_base;
+
+	class Exception : public ::Genode::Exception { };
+	Backend_server_base &backend_init_server();
+	Backend_client_base &backend_init_client();
+};
+
+struct Remote_rom::Backend_server_base
+{
+	virtual void send_update() = 0;
+	virtual void register_forwarder(Rom_forwarder_base *forwarder) = 0;
+};
+
+struct Remote_rom::Backend_client_base
+{
+	virtual void register_receiver(Rom_receiver_base *receiver) = 0;
+};
+
+#endif
---
+++ include/remote_rom/rom_forwarder.h
@@ -0,0 +1,24 @@
+/*
+ * \brief  Interface used by the backend to transfer ROM content to the remote clients.
+ * \author Johannes Schlatow
+ * \date   2016-02-17
+ */
+
+#ifndef __INCLUDE__REMOTE_ROM__ROM_FORWARDER_H_
+#define __INCLUDE__REMOTE_ROM__ROM_FORWARDER_H_
+
+#include <base/stdint.h>
+
+namespace Remote_rom {
+	using Genode::size_t;
+	struct Rom_forwarder_base;
+}
+
+struct Remote_rom::Rom_forwarder_base
+{
+	virtual const char *module_name() const = 0;
+	virtual size_t content_size() const = 0;
+	virtual size_t transfer_content(char *dst, size_t dst_len, size_t offset=0) const = 0;
+};
+
+#endif
